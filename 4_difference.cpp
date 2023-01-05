3e6186028677e562f0b445f86423ff033e05f10f
Maati_Mohamed <maati4455@gmail.com>
2023-01-05 20:44:09 +0200

    basic problem

---
+++ 4_difference.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long a,b,c,d,value;
+    cin>>a>>b>>c>>d;
+    value = (a*b)-(c*d);
+    cout<< "Difference = "<<value;
+    return 0;
+}
+
